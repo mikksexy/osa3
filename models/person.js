449ca8872c034363351d30fea20d40002de2c84b
mikksexy <samihuehue@gmail.com>
2021-12-09 19:28:22 +0200

    uibuild

--- models/person.js
+++ models/person.js
@@ -15,12 +15,13 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: 5,
+    minlength: 3,
     required: true,
     unique: true
   },
   number: {
     type: String,
+    minlength: 8,
     required: true
   }
 })
